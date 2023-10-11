da84831c5cc324149bed5d676e2431f10381fc58
semakeremetci <keremetcisema@gmail.com>
2023-10-11 17:43:26 +0300

    some change

--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -1,41 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
+const Companies = [
+  {
+    id: "company-01",
+    name: "Firma 1",
+  },
+  {
+    id: "company-02",
+    name: "Firma 2",
+  },
+  {
+    id: "company-03",
+    name: "Firma 3",
+  },
+  {
+    id: "company-04",
+    name: "Firma 4",
+  },
+  {
+    id: "company-05",
+    name: "Firma 5",
+  },
+];
+
 function Section() {
-  const Companies = [
-    {
-      id: "company-01",
-      name: "Firma 1",
-    },
-    {
-      id: "company-02",
-      name: "Firma 2",
-    },
-    {
-      id: "company-03",
-      name: "Firma 3",
-    },
-    {
-      id: "company-04",
-      name: "Firma 4",
-    },
-    {
-      id: "company-05",
-      name: "Firma 5",
-    },
-  ];
+  const localStorageItems = JSON.parse(localStorage.getItem("companies"));
+  const [companies, updateCompanies] = useState(
+    localStorageItems ? localStorageItems : Companies
+  );
+
+  const handleOnDragEnd = (result) => {
+    // console.log(result);
+    const items = Array.from(companies);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    updateCompanies(items);
+
+    localStorage.setItem("companies", JSON.stringify(items));
+  };
 
   return (
     <div className="sm:mx-32 my-8">
-      <DragDropContext>
-        <Droppable droppableId="character">
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="company">
           {(provided) => (
             <ul
               className="sm:p-4 p-2 text-black flex flex-col gap-4"
               {...provided.droppableProps}
               ref={provided.innerRef}
             >
-              {Companies.map((company, index) => {
+              {companies.map((company, index) => {
                 return (
                   <Draggable
                     draggableId={company.id}
@@ -68,6 +83,7 @@ function Section() {
                   </Draggable>
                 );
               })}
+              {provided.placeholder}
             </ul>
           )}
         </Droppable>
